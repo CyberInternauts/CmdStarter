d132ae48697f14af93a824a2269939089431f220
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-16 10:56:00 +0100

    CMD-2 Add helper classes

---
+++ CmdStarter.Tests.Commands/Filtering/Starter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Filtering
+{
+    public sealed class Starter : StarterCommand
+    {
+    }
+
+    public sealed class StarterA : StarterCommand
+    {
+    }
+
+    public sealed class StarterB : StarterCommand
+    {
+    }
+
+    public sealed class StarterOn : StarterCommand
+    {
+    }
+
+    public sealed class StarterOff : StarterCommand
+    {
+    }
+}
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Filtering.A.IO
+{
+    public sealed class Starter : StarterCommand
+    {
+    }
+}
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Filtering.B.IO
+{
+    public sealed class Starter : StarterCommand
+    {
+    }
+}
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Filtering.NSouth
+{
+    public sealed class Starter : StarterCommand
+    {
+    }
+}
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Filtering.EastNS
+{
+    public sealed class Starter : StarterCommand
+    {
+    }
+}
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Filtering.NorthSouth
+{
+    public sealed class Starter : StarterCommand
+    {
+    }
+}
