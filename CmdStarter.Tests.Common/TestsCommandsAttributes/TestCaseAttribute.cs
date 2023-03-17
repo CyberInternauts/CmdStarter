4af90822b7bfbdd9cc36fd3dc93635977768dad7
Jonathan Boivin <djon2003@hotmail.com>
2023-03-16 22:15:40 -0400

    CMD-7 Added generic TestCase attribute

---
+++ CmdStarter.Tests.Common/TestsCommandsAttributes/TestCaseAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Common.TestsCommandsAttributes
+{
+    /// <summary>
+    /// New version of <see cref="NUnit.Framework.TestCaseAttribute"/> that supports a generic parameter
+    /// </summary>
+    /// <seealso href="https://stackoverflow.com/a/43339950/214898">Class taken from</seealso>
+    /// <typeparam name="T"></typeparam>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class TestCaseAttribute<T> : TestCaseGenericAttribute
+    {
+        public TestCaseAttribute(params object[] arguments)
+            : base(arguments) => TypeArguments = new[] { typeof(T) };
+    }
+}
