023ea15ecd24853319066e13e15e12ee9b300941
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-21 20:52:27 +0100

    CMD-2 Add namespace filtering to test
    Added namespace filter to UsesClassesInclusion_WithinNamespace_WithoutWildCard test method

--- CmdStarter.Tests/Basics.cs
+++ CmdStarter.Tests/Basics.cs
@@ -225,9 +225,8 @@ namespace com.cyberinternauts.csharp.CmdStarter.Tests
         [TestCaseSource(nameof(FilterClasses))]
         [Category("Classes")]
         [Category("Filters")]
-        public void UsesClassesInclusion_WithinNamespace_WithoutWildCard(IEnumerable<Type> types)
+        public void UsesClassesInclusion_WithinNamespace_WithoutWildCard(string namespaceFilter, IEnumerable<Type> types)
         {
-            const string MainNamespaceFilter = nameof(Commands.Filtering) + "**.";
             const string IncludedClassName = nameof(Commands.Filtering.Starter);
             string includedNamespace = nameof(Commands.Filtering.A.IO).Split(".").Last();
             string finalFilter = $"{includedNamespace}.{IncludedClassName}";
@@ -236,7 +235,8 @@ namespace com.cyberinternauts.csharp.CmdStarter.Tests
 
             IEnumerable<Type> expectedTypes = types.Where(t => matcher.IsMatch(t.FullName ?? string.Empty));
 
-            starter.Classes = starter.Classes.Add(MainNamespaceFilter + finalFilter);
+            starter.Namespaces = starter.Namespaces.Add(namespaceFilter);
+            starter.Classes = starter.Classes.Add(finalFilter);
             starter.FindCommandsTypes();
 
             TestsCommon.AssertIEnumerablesHaveSameElements(expectedTypes, starter.CommandsTypes);
