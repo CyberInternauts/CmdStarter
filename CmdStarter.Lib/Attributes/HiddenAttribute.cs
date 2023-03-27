2b542edd5e6f65f44f90395967fca0738cd66666
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-27 19:28:35 +0200

    CMD-23 Fix allow multiple

--- CmdStarter.Lib/Attributes/HiddenAttribute.cs
+++ CmdStarter.Lib/Attributes/HiddenAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Parameter)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = false)]
     public sealed class HiddenAttribute : Attribute
     {
     }
