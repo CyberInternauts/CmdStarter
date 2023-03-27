199da524eb2bd979ad8426cd7948154de19c3d9e
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-27 19:57:33 +0200

    CMD-23 Add more test case helpers

---
+++ CmdStarter.Tests.Commands/Attributes/Hidden/PartlyHiddenCommandA.cs
@@ -0,0 +1,17 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Attributes.Hidden
+{
+    [Hidden]
+    public sealed class PartlyHiddenCommandA : StarterCommand
+    {
+        public const bool COMMAND_IS_HIDDEN = true;
+
+        public override Delegate MethodForHandling => (int parameter) => { };
+        public const string NAME_OF_PARAMETER = "parameter";
+        public const bool PARAMETER_IS_HIDDEN = false;
+
+        [Hidden]
+        public bool Option { get; set; }
+        public const string NAME_OF_OPTION = "option";
+        public const bool OPTION_IS_HIDDEN = true;
+    }
+}
---
+++ CmdStarter.Tests.Commands/Attributes/Hidden/PartlyHiddenCommandB.cs
@@ -0,0 +1,15 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Attributes.Hidden
+{
+    public sealed class PartlyHiddenCommandB : StarterCommand
+    {
+        public const bool COMMAND_IS_HIDDEN = false;
+
+        public override Delegate MethodForHandling => ([Hidden] int parameter) => { };
+        public const string NAME_OF_PARAMETER = "parameter";
+        public const bool PARAMETER_IS_HIDDEN = true;
+
+        public bool Option { get; set; }
+        public const string NAME_OF_OPTION = "option";
+        public const bool OPTION_IS_HIDDEN = false;
+    }
+}
