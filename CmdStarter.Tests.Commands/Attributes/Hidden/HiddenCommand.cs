e397be1ec35fe8ff40857420e72f7060a3b435e2
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-27 19:24:35 +0200

    CMD-23 Create helper classes

---
+++ CmdStarter.Tests.Commands/Attributes/Hidden/HiddenCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Attributes.Hidden
+{
+    [Hidden]
+    public sealed class HiddenCommand : StarterCommand
+    {
+        public override Delegate MethodForHandling => ([Hidden] int hiddenParameter) => { };
+        public const string NAME_OF_HIDDEN_PARAMETER = "hiddenParameter";
+
+        [Hidden]
+        public bool HiddenOption { get; set; }
+        public const string NAME_OF_HIDDEN_OPTION = "hidden-option";
+    }
+}
---
+++ CmdStarter.Tests.Commands/Attributes/Hidden/VisibleCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Attributes.Hidden
+{
+
+    public sealed class VisibleCommand : StarterCommand
+    {
+        public override Delegate MethodForHandling => (int visibleParameter) => { };
+        public const string NAME_OF_VISIBLE_PARAMETER = "visibleParameter";
+
+        public bool VisibleOption { get; set; }
+        public const string NAME_OF_VISIBLE_OPTION = "visible-option";
+    }
+}
