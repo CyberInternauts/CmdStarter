60b0e21fca806bc00d73c81e71b939a6a3ad948b
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-27 18:59:23 +0200

    CMD-23 Create HiddenAttribute

---
+++ CmdStarter.Lib/Attributes/HiddenAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Parameter)]
+    public sealed class HiddenAttribute : Attribute
+    {
+    }
+}
