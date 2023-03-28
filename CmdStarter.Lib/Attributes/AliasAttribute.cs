88a79a0ebdbcf3bbb731f11728add63eba0baeb0
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-03-28 22:49:38 +0200

    CMD-9 Add [Alias] attribute

---
+++ CmdStarter.Lib/Attributes/AliasAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
+    public sealed class AliasAttribute : Attribute
+    {
+        public static string DEFAULT_PREFIX = "-";
+
+        private readonly string _alias;
+
+        public string Alias => _alias;
+
+        public AliasAttribute(string alias)
+            : this(alias, DEFAULT_PREFIX)
+        { }
+
+        public AliasAttribute(string alias, string prefix)
+        {
+            _alias = prefix + alias;
+        }
+    }
+}
