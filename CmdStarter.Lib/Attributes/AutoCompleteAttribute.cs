ff07e41b17e2c779bbf04e8197c5ed741aa2dcf0
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-04-03 00:23:50 +0200

    CMD-14 Create [AutoCompletion]

---
+++ CmdStarter.Lib/Attributes/AutoCompleteAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.CommandLine.Completions;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = true)]
+    public class AutoCompleteAttribute : Attribute
+    {
+        private readonly CompletionItem[] _items;
+
+        public CompletionDelegate Context => (CompletionContext ctx) => _items;
+
+        public AutoCompleteAttribute(params object[] completions)
+        {
+            _items = new CompletionItem[completions.Length];
+
+            for (int i = 0; i < completions.Length; i++)
+            {
+                var autoCompleteValue = completions[i];
+
+                if (autoCompleteValue is null) continue;
+
+                CompletionItem completionItem = new(label: autoCompleteValue.ToString() ?? string.Empty);
+                _items[i] = completionItem;
+            }
+        }
+    }
+}
