39ec8ed1834290c48baa89773336d86020b019fb
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-04-03 17:35:30 +0200

    CMD-14 Create IEnumerableExtensions class

---
+++ CmdStarter.Lib/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Lib.Extensions
+{
+    internal static class IEnumerableExtensions
+    {
+        internal static object[] ToObjectArray<T>(this IEnumerable<T> enumerable)
+        {
+            return ToNullableObjectArray(enumerable)!;
+        }
+
+        internal static object?[] ToNullableObjectArray<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.Select(item => (object?)item).ToArray();
+        }
+    }
+}
