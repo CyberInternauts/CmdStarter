350db8ebe4f2e4216c082a103286c6e5842a1f18
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-04-03 17:36:17 +0200

    CMD-14 Add new test cases

---
+++ CmdStarter.Tests.Commands/Attributes/AutoComplete/SimpleConstructor/GenericMultiSingleAutoComplete .cs
@@ -0,0 +1,47 @@
+﻿using com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Interfaces;
+using System.CommandLine;
+using System.CommandLine.Completions;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Attributes.AutoComplete.SimpleConstructor
+{
+    public sealed class GenericMultiSingleAutoComplete : StarterCommand, IHasAutoComplete
+    {
+        private const string OPTION_COMPLETION_1 = "Bob";
+        private const string OPTION_COMPLETION_2 = "Robert";
+
+        private const int ARGUMENT_COMPLETION_1 = 18;
+        private const int ARGUMENT_COMPLETION_2 = 60;
+
+        [AutoComplete<string>(OPTION_COMPLETION_1)]
+        [AutoComplete<string>(OPTION_COMPLETION_2)]
+        public string PersonName { get; set; } = null!;
+        public const string OPTION_NAME = "person-name";
+
+        public override Delegate MethodForHandling => Execute;
+
+        private void Execute(
+            [AutoComplete<int>(ARGUMENT_COMPLETION_1)]
+            [AutoComplete<int>(ARGUMENT_COMPLETION_2)]
+            int age = 18)
+        { }
+        public const string ARGUMENT_NAME = "age";
+
+        public IEnumerable<CompletionItem> OptionExpected()
+        {
+            Option<string> expected = new(OPTION_NAME);
+
+            expected.AddCompletions(OPTION_COMPLETION_1, OPTION_COMPLETION_2);
+
+            return expected.GetCompletions();
+        }
+
+        public IEnumerable<CompletionItem> ArgumentExpected()
+        {
+            Argument<int> expected = new();
+
+            expected.AddCompletions(ARGUMENT_COMPLETION_1.ToString(), ARGUMENT_COMPLETION_2.ToString());
+
+            return expected.GetCompletions();
+        }
+    }
+}
