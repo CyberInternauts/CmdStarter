3b1441065c0e45541b621e844985d5e0ba7ea051
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-04-03 21:08:30 +0200

    CMD-14 Create new test case

---
+++ CmdStarter.Tests.Commands/Attributes/AutoComplete/Factories/AutoCompleteFactories.cs
@@ -0,0 +1,38 @@
+﻿using com.cyberinternauts.csharp.CmdStarter.Lib.Extensions;
+using com.cyberinternauts.csharp.CmdStarter.Lib.Interfaces;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Attributes.AutoComplete.Factories
+{
+    internal sealed class AutoCompleteOptionFactory : IAutoCompleteFactory<string>
+    {
+        public const string DETAIL = "Bob is a cool guy.";
+
+        public Func<string, string> LabelFactory => (str) => str.PascalToKebabCase();
+
+        public Func<string, string?> DetailFactory => (str) => DETAIL;
+
+        public Func<string, string?> InsertFactory => (str) => str.ToUpper();
+
+        public static IAutoCompleteFactory<string> GetDefault() => new AutoCompleteOptionFactory();
+    }
+
+    internal sealed class AutoCompleteArgumentFactory : IAutoCompleteFactory<int>
+    {
+        public const string DOCUMENTATION_EU = "Age of legal alcohol usage in the E.U.";
+        public const string DOCUMENTATION_USA = "Age of legal alcohol usage in the U.S.A.";
+        public const string DOCUMENTATION_NONE = "Cannot drink alcohol!";
+
+        public Func<int, string> LabelFactory => (num) => num.ToString();
+
+        public Func<int, string?> DocumentationFactory => (num) =>
+        {
+            if (num >= 21) return DOCUMENTATION_USA;
+
+            if (num >= 18) return DOCUMENTATION_EU;
+
+            return DOCUMENTATION_NONE;
+        };
+
+        public static IAutoCompleteFactory<int> GetDefault() => new AutoCompleteArgumentFactory();
+    }
+}
---
+++ CmdStarter.Tests.Commands/Attributes/AutoComplete/Factories/FactoryAutoComplete.cs
@@ -0,0 +1,48 @@
+﻿using com.cyberinternauts.csharp.CmdStarter.Lib.Extensions;
+using com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Interfaces;
+using System.CommandLine;
+using System.CommandLine.Completions;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Attributes.AutoComplete.Factories
+{
+    public sealed class FactoryAutoComplete : StarterCommand, IHasAutoComplete
+    {
+        private const string OPTION_COMPLETION_1 = "BobOrFrank";
+
+        private const int ARGUMENT_COMPLETION_1 = 18;
+
+        [AutoComplete<string, AutoCompleteOptionFactory>(OPTION_COMPLETION_1)]
+        public string PersonName { get; set; } = null!;
+        public const string OPTION_NAME = "person-name";
+
+        public override Delegate MethodForHandling => Execute;
+
+        private void Execute([AutoComplete<int, AutoCompleteArgumentFactory>(ARGUMENT_COMPLETION_1)] int age = 18)
+        { }
+        public const string ARGUMENT_NAME = "age";
+
+        public IEnumerable<CompletionItem> OptionExpected()
+        {
+            Option<string> expected = new(OPTION_NAME);
+
+            var item = new CompletionItem(OPTION_COMPLETION_1.PascalToKebabCase());
+
+            expected.AddCompletions((ctx) => new CompletionItem[] { item });
+
+            return expected.GetCompletions();
+        }
+
+        public IEnumerable<CompletionItem> ArgumentExpected()
+        {
+            Argument<int> expected = new();
+
+            var item = new CompletionItem(
+                ARGUMENT_COMPLETION_1.ToString(),
+                documentation: AutoCompleteArgumentFactory.DOCUMENTATION_EU);
+
+            expected.AddCompletions((ctx) => new CompletionItem[] { item });
+
+            return expected.GetCompletions();
+        }
+    }
+}
