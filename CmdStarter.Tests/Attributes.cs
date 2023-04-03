ea505b123c884d3e66bdecc74a34e17db3573166
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-04-03 21:19:47 +0200

    CMD-14 Create new test class

---
+++ CmdStarter.Tests/Attributes.cs
@@ -0,0 +1,22 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests
+{
+    [Category("* All *")]
+    [Category("CmdStarter")]
+    [Category("Attributes")]
+    public class Attributes
+    {
+        private Lib.Starter starter;
+
+        [OneTimeSetUp]
+        public void GlobalSetup()
+        {
+            TestsCommon.GlobalSetup();
+        }
+
+        [SetUp]
+        public void MethodSetup()
+        {
+            starter = TestsCommon.CreateCmdStarter();
+        }
+    }
+}
