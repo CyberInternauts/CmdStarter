6a213365dddb00faaa75fb4b148bfa5160a3f22a
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-04-03 19:55:03 +0200

    CMD-14 Create new type of AutoCompleteAttribute

---
+++ CmdStarter.Lib/Attributes/AutoComplete/AutoCompleteAttribute{CompletionT,FactoryT}.cs
@@ -0,0 +1,40 @@
+﻿using com.cyberinternauts.csharp.CmdStarter.Lib.Attributes.AutoComplete;
+using System.CommandLine.Completions;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
+{
+    public sealed class AutoCompleteAttribute<CompletionT, FactoryT> : AutoCompleteAttribute<CompletionT>
+        where FactoryT : IAutoCompleteFactory<CompletionT>
+    {
+        public AutoCompleteAttribute(params CompletionT[] completions)
+            : base(completions)
+        { }
+
+        protected override void CacheItems()
+        {
+            _items = new CompletionItem[_objects.Length];
+
+            var factory = FactoryT.GetDefault();
+
+            for (int i = 0; i < _items.Length; i++)
+            {
+                var item = (CompletionT)_objects[i];
+
+                var label = factory.LabelFactory(item);
+                var sortText = factory.SortTextFactory(item);
+                var insertText = factory.InsertTextFactory(item);
+                var documentation = factory.DocumentationFactory(item);
+                var detail = factory.DetailFactory(item);
+
+                var completionItem = new CompletionItem(
+                    label,
+                    sortText: sortText,
+                    insertText: insertText,
+                    documentation: documentation,
+                    detail: detail);
+
+                _items[i] = completionItem;
+            }
+        }
+    }
+}
