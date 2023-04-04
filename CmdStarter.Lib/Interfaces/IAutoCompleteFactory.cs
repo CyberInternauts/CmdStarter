c158c32d9336b963b6abb4d70f119a382d445717
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-04-04 21:02:32 +0200

    CMD-14 Change IAutoCompleteFactory

--- CmdStarter.Lib/Interfaces/IAutoCompleteFactory.cs
+++ CmdStarter.Lib/Interfaces/IAutoCompleteFactory.cs
@@ -1,34 +1,27 @@
-﻿using com.cyberinternauts.csharp.CmdStarter.Lib.Attributes;
-
-namespace com.cyberinternauts.csharp.CmdStarter.Lib.Interfaces
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Lib.Interfaces
 {
-    public interface IAutoCompleteFactory<T>
+    public interface IAutoCompleteFactory
     {
-        /// <summary>
-        /// Gets the label value, which is the text displayed to users and, unless <see cref="GetInsertText"/> is set, is also used to populate the <see cref="CompletionItem.InsertText"/> property.
-        /// </summary>
-        string GetLabel(T value);
-
         /// <summary>
         /// Gets the value used to sort the completion item in a list. If this is not provided, then <see cref="GetLabel"/> is used.
         /// </summary>
-        string? GetSortText(T value) => null;
+        string? GetSortText(string value) => null;
 
         /// <summary>
         /// Gets the text to be inserted by this completion item. If this is not provided, then <see cref="GetLabel"/> is used.
         /// </summary>
-        string? GetInsertText(T value) => null;
+        string? GetInsertText(string value) => null;
 
         /// <summary>
         /// Gets documentation about the completion item.
         /// </summary>
-        string? GetDocumentation(T value) => null;
+        string? GetDocumentation(string value) => null;
 
         /// <summary>
         /// Generates additional details regarding the completion item.
         /// </summary>
-        string? GetDetail(T value) => null;
+        string? GetDetail(string value) => null;
 
-        static abstract IAutoCompleteFactory<T> GetDefault();
+        static abstract IAutoCompleteFactory GetDefault();
     }
 }
