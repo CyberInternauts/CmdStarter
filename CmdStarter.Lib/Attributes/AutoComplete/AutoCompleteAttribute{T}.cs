debddd29b5ab24389c16617c3ce45ab433a777b4
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-04-05 13:23:11 +0200

    CMD-14 Remove merged features

--- CmdStarter.Lib/Attributes/AutoComplete/AutoCompleteAttribute{T}.cs
+++ CmdStarter.Lib/Attributes/AutoComplete/AutoCompleteAttribute{T}.cs
@@ -1,5 +1,4 @@
 ﻿using com.cyberinternauts.csharp.CmdStarter.Lib.Interfaces;
-using System.CommandLine.Completions;
 
 namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
 {
@@ -13,8 +12,6 @@ namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = true)]
     public sealed class AutoCompleteAttribute<T> : AutoCompleteAttribute
     {
-        private readonly IAutoCompleteFactory? _factory;
-
         /// <summary>
         /// <para>
         /// If <typeparamref name="T"/> is <see langword="typeof"/> <see cref="Enum"/>
@@ -31,61 +28,12 @@ namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
 
         /// <inheritdoc cref="AutoCompleteAttribute(object[])"/>
         public AutoCompleteAttribute(params object[] completions)
-            : base(completions)
-        {
-            _factory = GetFactory();
-        }
+            : base(typeof(T), completions)
+        { }
 
         /// <inheritdoc cref="AutoCompleteAttribute(Type)"/>
-        public AutoCompleteAttribute(Type type)
-            : base(type)
-        {
-            _factory = GetFactory();
-        }
-
-        protected sealed override void CacheItems()
-        {
-            if (_factory is null)
-            {
-                base.CacheItems();
-                return;
-            }
-
-            _items = new LinkedList<CompletionItem>();
-
-            for (int i = 0; i < _labels.Length; i++)
-            {
-                var label = _labels[i];
-
-                var sortText = _factory.GetSortText(label);
-                var insertText = _factory.GetInsertText(label);
-                var documentation = _factory.GetDocumentation(label);
-                var detail = _factory.GetDetail(label);
-
-                var completionItem = new CompletionItem(
-                    label,
-                    sortText: sortText,
-                    insertText: insertText,
-                    documentation: documentation,
-                    detail: detail);
-
-                _items.AddLast(completionItem);
-            }
-        }
-
-        private static IAutoCompleteFactory GetFactory()
-        {
-            const string NOT_ASSIGNABLE_ERROR_MESSAGE = "{0} is not assignable from IAutoCompleteFactory.";
-
-            if (!typeof(T).IsAssignableTo(typeof(IAutoCompleteFactory)))
-            {
-                var message = string.Format(NOT_ASSIGNABLE_ERROR_MESSAGE, typeof(T));
-                throw new InvalidCastException(message);
-            }
-
-            var getDefaultMethod = typeof(T).GetMethod(nameof(IAutoCompleteFactory.GetDefault))!; //Cannot be null as implementation is required.
-
-            return (IAutoCompleteFactory)getDefaultMethod.Invoke(null, null)!; //Implementation requires non-nullable return.
-        }
+        public AutoCompleteAttribute(Type provider)
+            : base(typeof(T), provider)
+        { }
     }
 }
