11f47f084abd4a052e18b3280bc3c5d40f7cfeb2
Jonathan Boivin <djon2003@hotmail.com>
2023-05-04 23:52:30 -0400

    Ensure namespace corresponds to folder tree

--- CmdStarter.Lib/Attributes/AutoCompleteAttribute.cs
+++ CmdStarter.Lib/Attributes/AutoCompleteAttribute.cs
@@ -71,8 +71,8 @@ namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
             _labels = new LinkedList<string>();
             FillLabels(completions);
 
-            const string EXCEPTION_MESSAGE = "The parameter " + nameof(features) + " must be an Enum or implement " 
-                + nameof(IAutoCompleteProvider)  + " or " + nameof(IAutoCompleteFactory)
+            const string EXCEPTION_MESSAGE = "The parameter " + nameof(features) + " must be an Enum or implement "
+                + nameof(IAutoCompleteProvider) + " or " + nameof(IAutoCompleteFactory)
                 + " and having at least one completion";
 
             bool isSupported = false;
@@ -144,5 +144,48 @@ namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
                 _items.AddLast(completionItem);
             }
         }
+    }
+
+
+    /// <inheritdoc cref="AutoCompleteAttribute"/>
+    /// <typeparam name="T">
+    /// Defines the behaviour of the attribute.
+    /// <para>
+    /// More info in constructors.
+    /// </para>
+    /// </typeparam>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = true)]
+    public sealed class AutoCompleteAttribute<T> : AutoCompleteAttribute
+    {
+        /// <summary>
+        /// <para>
+        /// If <typeparamref name="T"/> is <see langword="typeof"/> <see cref="Enum"/>
+        /// generates auto completions from all values. 
+        /// </para>
+        /// <para>
+        /// If <typeparamref name="T"/> is <see langword="typeof"/> <see cref="IAutoCompleteProvider"/>
+        /// retrieves auto completions from there.
+        /// </para>
+        /// <para>
+        /// Can also be <see langword="typeof"/> <see cref="IAutoCompleteFactory"/> besides these.
+        /// </para>
+        /// </summary>
+        public AutoCompleteAttribute()
+            : base(typeof(T))
+        { }
+
+        /// <summary>
+        /// Creates auto completions from the given <paramref name="completions"/> and handles <typeparamref name="T"/>.
+        /// <para>
+        /// <typeparamref name="T"/> must be <see langword="typeof"/> <see cref="IAutoCompleteProvider"/>.
+        /// <para>
+        /// Can also be <see langword="typeof"/> <see cref="IAutoCompleteFactory"/>.
+        /// </para>
+        /// </para>
+        /// </summary>
+        /// <param name="completions">Labels for the auto completions.</param>
+        public AutoCompleteAttribute(params object[] completions)
+            : base(typeof(T), completions)
+        { }
     }
 }
