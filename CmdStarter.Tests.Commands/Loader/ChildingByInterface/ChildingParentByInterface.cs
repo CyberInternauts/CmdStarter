dc3708edb04ef125371a55e8510f28735a80efba
Jonathan Boivin <djon2003@hotmail.com>
2023-05-04 23:42:46 -0400

    CMD-6 Add childing test path by interfaces

---
+++ CmdStarter.Tests.Commands/Loader/ChildingByInterface/ChildingParentByInterface.cs
@@ -0,0 +1,12 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Loader.ChildingByInterface
+{
+    public class ChildingParentByInterface : IStarterCommand
+    {
+        public GlobalOptionsManager? GlobalOptionsManager { get; set; }
+
+        public Delegate HandlingMethod => () => { };
+
+        public static IStarterCommand GetInstance<CommandType>() where CommandType : IStarterCommand
+            => new ChildingParentByInterface();
+    }
+}
