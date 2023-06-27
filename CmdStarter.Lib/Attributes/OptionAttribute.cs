c6cb8d5699a186642b10ac6deb5d0e94bbe89086
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-06-27 21:12:38 +0200

    CMD-25 Create attribute classes

---
+++ CmdStarter.Lib/Attributes/OptionAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class OptionAttribute : Attribute
+    {
+    }
+}
