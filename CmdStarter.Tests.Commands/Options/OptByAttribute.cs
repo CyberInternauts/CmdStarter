403547d50bceaa97d345f6c9b8462042db23bc33
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-06-27 21:12:29 +0200

    CMD-25 Create test classes

---
+++ CmdStarter.Tests.Commands/Options/OptByAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Options
+{
+    public sealed class OptByAttribute : StarterCommand
+    {
+        public int OptionToInclude { get; set; }
+
+        public int OptionToExclude { get; set; }
+
+        public int OptionToIgnore { get; set; }
+    }
+}
