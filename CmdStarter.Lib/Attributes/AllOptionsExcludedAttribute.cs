592c2d9b3c58e7cfa89639280f2013c9210fef35
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-06-29 20:29:50 +0200

    CMD-25 Create class exclude attribute

---
+++ CmdStarter.Lib/Attributes/AllOptionsExcludedAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Lib.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
+    public sealed class AllOptionsExcludedAttribute : Attribute
+    {
+    }
+}
