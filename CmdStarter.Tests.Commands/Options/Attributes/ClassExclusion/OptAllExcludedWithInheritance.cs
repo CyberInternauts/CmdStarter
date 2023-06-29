0cb95293d81a4820a11823407f90e3c652be226a
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-06-29 19:55:11 +0200

    CMD-25 Create test cases

---
+++ CmdStarter.Tests.Commands/Options/Attributes/ClassExclusion/OptAllExcludedWithInheritance.cs
@@ -0,0 +1,9 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Options.Attributes.ClassExclusion
+{
+    public sealed class OptAllExcludedWithInheritance : OptAllPropertiesExcluded
+    {
+        public new const int OPTION_COUNT = 1;
+
+        public int IncludedOption { get; set; }
+    }
+}
---
+++ CmdStarter.Tests.Commands/Options/Attributes/ClassExclusion/OptAllPropertiesExcluded.cs
@@ -0,0 +1,13 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Options.Attributes.ClassExclusion
+{
+    [AllOptionsExcluded]
+    public class OptAllPropertiesExcluded : StarterCommand
+    {
+        public const int OPTION_COUNT = 0;
+
+        public int OptionOne { get; set; }
+        public int OptionTwo { get; set; }
+        public int OptionThree { get; set; }
+        public int OptionFour { get; set; }
+    }
+}
