bd89ddd02b281436383511e7063a65498c751d6a
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-06-29 14:49:39 +0200

    CMD-25 Add new Interface

---
+++ CmdStarter.Tests.Commands/Options/Attributes/IOptByAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Options.Attributes
+{
+    public interface IOptByAttribute
+    {
+        static abstract string[] IncludedOptions { get; }
+        static abstract string[] ExcludedOptions { get; }
+    }
+}
