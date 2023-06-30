2b6c6907cbf323db6e6d8eed69a50e95a63210e9
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-06-30 22:44:17 +0200

    CMD-1 Create new project

---
+++ CmdStarter.Demo.Repl/Program.cs
@@ -0,0 +1,23 @@
+﻿using com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Demo;
+using System.ComponentModel.Composition.Hosting;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Demo.Repl
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            _ = new DirectoryCatalog("."); // This is needed to ensure Tests.Commands assembly is loaded even if no class referenced
+
+            var starter = new CmdStarter.Lib.Starter(
+                new string[] {
+                    typeof(List).Namespace!, // Only accepts Demo commands
+                    typeof(Tests.Commands.GlobalOptions.MainGlobalOptions).Namespace!,
+                }
+            );
+            await starter.Start(args);
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
