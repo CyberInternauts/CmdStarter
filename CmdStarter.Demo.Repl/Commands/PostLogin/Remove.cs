803d6c71ef51a0e9e031de2c97a14a753fc6343b
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-06-30 23:13:16 +0200

    CMD-1 Create demo

---
+++ CmdStarter.Demo.Repl/Commands/PostLogin/Remove.cs
@@ -0,0 +1,25 @@
+﻿using com.cyberinternauts.csharp.CmdStarter.Lib;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Demo.Repl.Commands.PostLogin
+{
+    public sealed class Remove : StarterCommand
+    {
+        public override Delegate HandlingMethod => base.HandlingMethod;
+
+        public int Execute(
+            [Required][Description("The name of the book to be removed.")]
+            string name)
+        {
+            if(DemoDb.Books.Remove(name))
+            {
+                Console.WriteLine($"Book: {name} removed");
+                return 0;
+            }
+
+            Console.WriteLine($"Book: {name} doesn't exist.");
+            return -1;
+        }
+    }
+}
