bdab36c5ca7df904b79e431608e2fdc7d66b5b11
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-07-02 18:36:32 +0200

    CMD-1 Create test

---
+++ CmdStarter.Tests/Repl.cs
@@ -0,0 +1,23 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests
+{
+    [Category("* All *")]
+    [Category("CmdStarter")]
+    [Category("Commands")]
+    [Category("REPL")]
+    public class Repl
+    {
+        private Lib.Starter starter;
+
+        [OneTimeSetUp]
+        public void GlobalSetup()
+        {
+            TestsCommon.GlobalSetup();
+        }
+
+        [SetUp]
+        public void MethodSetup()
+        {
+            starter = TestsCommon.CreateCmdStarter();
+        }
+    }
+}
