82f299c9e5725234526917c4df0131b6195e8616
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-07-03 19:42:43 +0200

    CMD-1 Create Repl provider class

---
+++ CmdStarter.Lib/Extended/ConsoleReplProvider.cs
@@ -0,0 +1,19 @@
+﻿using com.cyberinternauts.csharp.CmdStarter.Lib.Interfaces;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Lib.Extended
+{
+    /// <summary>
+    /// Provides command inputs from the default console.
+    /// </summary>
+    public sealed class ConsoleReplProvider : IReplInputProvider
+    {
+        /// <summary>
+        /// Returns an input from the default console.
+        /// </summary>
+        /// <returns>A <see cref="string"/> with <see cref="Console.ReadLine"/>.</returns>
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
