506549b7d61d97aa999f5ebaec4742f110dc6b52
Norbert Ormándi <60075235+DeszkaCodes@users.noreply.github.com>
2023-07-03 22:20:41 +0200

    CMD-1 Add TestInputProvider

---
+++ CmdStarter.Tests.Commands/Repl/TestInputProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Repl
+{
+    public sealed class TestInputProvider : IReplInputProvider
+    {
+        public readonly Queue<string> CommandQueue = new Queue<string>();
+
+        public string GetInput()
+        {
+            var command = CommandQueue.Dequeue();
+            CommandQueue.Enqueue(command);
+
+            return command;
+        }
+    }
+}
