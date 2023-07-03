54e1211b3930b67bb98ede1afba84d66a028bb20
Norbert Ormándi <60075235+skydeszka@users.noreply.github.com>
2023-07-03 19:19:14 +0200

    CMD-1 Create input interface

---
+++ CmdStarter.Lib/Interfaces/IReplInputProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Lib.Interfaces
+{
+    /// <summary>
+    /// Provides an input for the REPL mode to parse.
+    /// </summary>
+    public interface IReplInputProvider
+    {
+        /// <summary>
+        /// Returns an input that can be parsed by the command.
+        /// </summary>
+        public string GetInput();
+    }
+}
