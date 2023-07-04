fcab85549a980f149f747086ac550b1d243e5191
Norbert Ormándi <60075235+DeszkaCodes@users.noreply.github.com>
2023-07-04 22:22:03 +0200

    CMD-1 Implement and add expected value interface

--- CmdStarter.Tests.Commands/Repl/CommandThree.cs
+++ CmdStarter.Tests.Commands/Repl/CommandThree.cs
@@ -1,10 +1,14 @@
-﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Repl
+﻿using com.cyberinternauts.csharp.CmdStarter.Tests.Common.Interfaces;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Repl
 {
-    public sealed class CommandThree : StarterCommand
+    public sealed class CommandThree : StarterCommand, IHasExpectedValue<int>
     {
         public const int EXPECTED_RETURN = 333;
 
         public override Delegate HandlingMethod => Execute;
+
+        public int ExpectedValue => EXPECTED_RETURN;
 
         public int Execute()
         {
--- CmdStarter.Tests.Commands/Repl/PreCommand.cs
+++ CmdStarter.Tests.Commands/Repl/PreCommand.cs
@@ -1,10 +1,14 @@
-﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Repl
+﻿using com.cyberinternauts.csharp.CmdStarter.Tests.Common.Interfaces;
+
+namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Repl
 {
-    public sealed class PreCommand : StarterCommand
+    public sealed class PreCommand : StarterCommand, IHasExpectedValue<int>
     {
         public const int EXPECTED_RETURN = -111;
 
         public override Delegate HandlingMethod => Execute;
+
+        public int ExpectedValue => EXPECTED_RETURN;
 
         public int Execute()
         {
