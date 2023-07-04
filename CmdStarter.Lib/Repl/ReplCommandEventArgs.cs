a342675e076257354d799349833edfe384384464
Norbert Ormándi <60075235+DeszkaCodes@users.noreply.github.com>
2023-07-04 20:30:30 +0200

    CMD-1 Remove unused Command property

--- CmdStarter.Lib/Repl/ReplCommandEventArgs.cs
+++ CmdStarter.Lib/Repl/ReplCommandEventArgs.cs
@@ -10,18 +10,12 @@ namespace com.cyberinternauts.csharp.CmdStarter.Lib.Repl
         /// <summary>
         /// Initializes a new instance of the <see cref="ReplCommandEventArgs"/> class.
         /// </summary>
-        /// <param name="command">The executed command.</param>
         /// <param name="returnCode">The return code of the command execution.</param>
-        public ReplCommandEventArgs(Command command, int returnCode)
+        public ReplCommandEventArgs(int returnCode)
         {
             Command = command;
             ReturnCode = returnCode;
         }
-
-        /// <summary>
-        /// Gets the executed command.
-        /// </summary>
-        public Command Command { get; }
 
         /// <summary>
         /// Gets the return code of the command execution.
