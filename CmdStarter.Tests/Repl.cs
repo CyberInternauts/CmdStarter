9b74aef66544d5e32ca09691a425b94ebd80b36a
Norbert Ormándi <60075235+DeszkaCodes@users.noreply.github.com>
2023-07-04 21:06:44 +0200

    CMD-1 Finish test

--- CmdStarter.Tests/Repl.cs
+++ CmdStarter.Tests/Repl.cs
@@ -3,6 +3,7 @@ using com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Repl;
 using com.cyberinternauts.csharp.CmdStarter.Lib.Repl;
 using com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Loader.DepencendyInjection;
 using com.cyberinternauts.csharp.CmdStarter.Lib.Extensions;
+using com.cyberinternauts.csharp.CmdStarter.Tests.Common.Interfaces;
 
 namespace com.cyberinternauts.csharp.CmdStarter.Tests
 {
@@ -14,6 +15,8 @@ namespace com.cyberinternauts.csharp.CmdStarter.Tests
     {
         public static ReplStarter starter;
         private TestInputProvider inputProvider = new();
+
+        private Queue<int> ensureExecutionTestReturnCodeQueue = new Queue<int>();
 
         [OneTimeSetUp]
         public void GlobalSetup()
@@ -41,8 +44,9 @@ namespace com.cyberinternauts.csharp.CmdStarter.Tests
             starter.Namespaces = starter.Namespaces.Add(TestsCommon.EXCLUSION_SYMBOL + typeof(Dependent1).Namespace!);
         }
 
-        [TestCase<CommandOne, CommandTwo, CommandThree>()]
-        public void EnsureExecution<CommandOne, CommandTwo, CommandThree>()
+        [TestCase<CommandOne, CommandTwo, CommandThree>(CommandOne.EXPECTED_RETURN, CommandTwo.EXPECTED_RETURN, CommandThree.EXPECTED_RETURN)]
+        public async Task EnsureExecution<CommandOne, CommandTwo, CommandThree>(
+            int expectedValueOne, int expectedValueTwo, int expectedValueThree)
             where CommandOne : StarterCommand
             where CommandTwo : StarterCommand
             where CommandThree : StarterCommand
@@ -50,11 +54,31 @@ namespace com.cyberinternauts.csharp.CmdStarter.Tests
             starter.Namespaces = starter.Namespaces.Add(typeof(CommandOne).Namespace!);
             starter.InstantiateCommands();
 
+            starter.OnCommandExecuted += Starter_OnCommandExecuted_Assert;
+
+            ensureExecutionTestReturnCodeQueue.Clear();
+            ensureExecutionTestReturnCodeQueue.Enqueue(expectedValueOne);
+            ensureExecutionTestReturnCodeQueue.Enqueue(expectedValueTwo);
+            ensureExecutionTestReturnCodeQueue.Enqueue(expectedValueThree);
+
             inputProvider.CommandQueue.Enqueue(typeof(CommandOne).Name.PascalToKebabCase());
             inputProvider.CommandQueue.Enqueue(typeof(CommandTwo).Name.PascalToKebabCase());
             inputProvider.CommandQueue.Enqueue(typeof(CommandThree).Name.PascalToKebabCase());
 
+            await starter.Launch();
+        }
 
+        private void Starter_OnCommandExecuted_Assert(object? sender, ReplCommandEventArgs e)
+        {
+            if(!ensureExecutionTestReturnCodeQueue.TryDequeue(out int expectedReturnCode))
+            {
+                starter.Stop();
+                return;
+            }
+
+            var actualReturnCode = e.ReturnCode;
+
+            Assert.That(actualReturnCode, Is.EqualTo(expectedReturnCode));
         }
     }
 }
