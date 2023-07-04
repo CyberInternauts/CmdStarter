b548d62245b5b390cdf9c216f8e1cbf768584cf3
Norbert Ormándi <60075235+DeszkaCodes@users.noreply.github.com>
2023-07-04 22:00:21 +0200

    CMD-1 Add new test command

---
+++ CmdStarter.Tests.Commands/Repl/PreCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace com.cyberinternauts.csharp.CmdStarter.Tests.Commands.Repl
+{
+    public sealed class PreCommand : StarterCommand
+    {
+        public const int EXPECTED_RETURN = -111;
+
+        public override Delegate HandlingMethod => Execute;
+
+        public int Execute()
+        {
+            return EXPECTED_RETURN;
+        }
+    }
+}
